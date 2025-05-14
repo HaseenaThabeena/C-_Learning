2b8e60dd8e7c5340154bd1a9bb1d34964f0dad4c
haseena <haseena@mes-india.in>
2025-05-14 10:02:58 +0530

    newinheritance

---
+++ C#Learning/Plotvalue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Learning
+{
+    internal class Plotvalue
+    {
+        public int length;
+        public int breath;
+        
+        public int areavale(int l,int b)
+        {
+            length = l;
+            breath = b;
+            return l * b;
+        }
+    }
+}
