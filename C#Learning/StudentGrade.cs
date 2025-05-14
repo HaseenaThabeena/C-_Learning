d4b8db757d0edcb7284fd5819a0fac83e4f7a878
haseena <haseena@mes-india.in>
2025-05-14 09:20:09 +0530

    student grade

--- C#Learning/StudentGrade.cs
+++ C#Learning/StudentGrade.cs
@@ -1,48 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_Learning
 {
     internal class StudentGrade
     {
-        public int total,grade;
-        public string name;
-        //class Students
-        //{
-        //    public string name;
-        //    public int grade;
-
-        //}
-        Dictionary<string,int> students = new Dictionary<string, int>();
+        public Dictionary<string, int> students = new Dictionary<string, int>();
 
         public void totalstudent()
         {
-            Console.WriteLine("Total no. of student");
-             total= Convert.ToInt32(Console.ReadLine());
-            addrecords();
-        }
-        
-        public void addrecords()
-        {
-            for (int i = 0; i <total; i++)
-            {
-                Console.Write("Enter student name: ");
-                string name = Console.ReadLine();
-                Console.Write("Enter student grade (0-100): ");
-                int grade = Convert.ToInt32(Console.ReadLine());
-            }
-            averagerecord();
-        }
-        public void averagerecord()
-        {
-            int avgrecord;
-            avgrecord = grade / total;
-            Console.Write(avgrecord);
-            Console.ReadKey();
+            Console.Write("Enter total number of students: ");
+            int total = Convert.ToInt32(Console.ReadLine());
+            AddRecords(total);
         }
 
+        private void AddRecords(int total)
+        {
+            for (int i = 0; i < total; i++)
+            {
+                Console.Write("Enter name for student : ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter grade for  (0-100): ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+
+                
+                students[name] = grade;
+            }
+
+            AverageRecord();
+        }
+
+        private void AverageRecord()
+        {
+            if (students.Count == 0)
+            {
+                Console.WriteLine("No student records available.");
+                return;
+            }
+
+            double average = 0;
+            foreach (var student in students)
+            {
+                average += student.Value;
+            }
+
+            average /= students.Count;
+
+            Console.WriteLine($"\nAverage Grade: {average:F2}");
+            Console.ReadKey();
+        }
     }
 }
