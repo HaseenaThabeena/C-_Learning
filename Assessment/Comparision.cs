3d51e19ba110206934306fdef6495be764448a33
haseena <haseena@mes-india.in>
2025-06-13 09:17:50 +0530

    Asse

---
+++ Assessment/Comparision.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    public class Comparision
+    {
+         Dictionary<string, int> scores = new Dictionary<string, int>
+         {
+            {"Arun", 78},
+            {"Kavya", 92},
+            {"Vimal", 92},
+            {"Sneha", 85}
+         };
+        int a;
+        public void compa()
+        {
+            a = scores.Values.Max();
+            Console.WriteLine("Highest Score" + a);
+            Console.WriteLine("Top Scorers:");
+            foreach (var item in scores)
+            {
+                if(a== item.Value)
+                Console.WriteLine(item.Key);
+            }   
+        }
+    }
+}
---
+++ Assessment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Groupwords group = new Groupwords();
+            //group.grouping();
+            //Triangle tri = new Triangle();
+            //tri.pattern();
+            Comparision com = new Comparision();
+            com.compa();
+        }
+    }
+}
